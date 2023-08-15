38ec8b932be4ec8f1a33d2c41e2f22b9285a8b73
Anderson Freitas <andersonfreitas21@gmail.com>
2023-08-15 19:46:10 -0300

    feat: find user by id

--- src/main/java/com/picpaysimplificado/services/UserService.java
+++ src/main/java/com/picpaysimplificado/services/UserService.java
@@ -2,25 +2,39 @@ package com.picpaysimplificado.services;
 
 import com.picpaysimplificado.domain.user.User;
 import com.picpaysimplificado.domain.user.UserType;
+import com.picpaysimplificado.exception.ResourceNotFoundException;
 import com.picpaysimplificado.repositories.UserRepository;
-import lombok.RequiredArgsConstructor;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 
 @Service
-@RequiredArgsConstructor
+@AllArgsConstructor
 public class UserService {
 
     private final UserRepository userRepository;
 
     public void validateTransaction(User sender, BigDecimal amount) throws Throwable {
-        if(sender.getUserType() == UserType.MERCHANT){
+        if (sender.getUserType() == UserType.MERCHANT) {
             throw new Exception("User is not authorized to perform transaction");
         }
 
-        if(sender.getBalance().compareTo(amount) < 0){
+        if (sender.getBalance().compareTo(amount) < 0) {
             throw new Exception("Insufficient funds");
         }
+    }
+
+    public User findUserById(Long id) {
+        var existingUser = this.userRepository.findById(id);
+
+        if (existingUser.isEmpty()) {
+            throw new ResourceNotFoundException("user not found");
+        }
+        return existingUser.get();
+    }
+
+    public void save(User user){
+        this.userRepository.save(user);
     }
 }
