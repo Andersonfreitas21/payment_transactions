07ffcd91123ec9b61aea1a5ad4d6f57fb35ec459
Anderson Freitas <andersonfreitas21@gmail.com>
2023-08-15 19:50:17 -0300

    feat: fixing custom search in UserRepository

--- src/main/java/com/picpaysimplificado/repositories/TransactionRepository.java
+++ src/main/java/com/picpaysimplificado/repositories/TransactionRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
-    Optional<User> findUserByDocument(String document);
+
 }
