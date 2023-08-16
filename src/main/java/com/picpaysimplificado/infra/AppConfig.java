ed8421db45982939fd0d035473dc8afc03339c60
Anderson Freitas <andersonfreitas21@gmail.com>
2023-08-16 18:41:49 -0300

    feat: Resilience4j config

--- src/main/java/com/picpaysimplificado/infra/AppConfig.java
+++ src/main/java/com/picpaysimplificado/infra/AppConfig.java
@@ -1,13 +1,32 @@
 package com.picpaysimplificado.infra;
 
+import io.github.resilience4j.retry.Retry;
+import io.github.resilience4j.retry.RetryConfig;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.IOException;
+import java.time.Duration;
+
 @Configuration
 public class AppConfig {
     @Bean
+    @Qualifier("resilienceRestTemplate")
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+
+    @Bean
+    public Retry retry() {
+        RetryConfig config = RetryConfig.custom()
+                .maxAttempts(3)
+                .waitDuration(Duration.ofMillis(100))
+                .retryExceptions(IOException.class)
+                .build();
+
+        return Retry.of("myRetry", config);
     }
 }
