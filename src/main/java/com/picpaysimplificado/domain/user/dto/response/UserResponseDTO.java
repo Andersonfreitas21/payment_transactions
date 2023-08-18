6173e8d5b548607c3a57fc90da5fcb5a6f192d77
Anderson Freitas <andersonfreitas21@gmail.com>
2023-08-18 18:08:41 -0300

    feat: adding id in UserResponseDTO

--- src/main/java/com/picpaysimplificado/domain/user/dto/response/UserResponseDTO.java
+++ src/main/java/com/picpaysimplificado/domain/user/dto/response/UserResponseDTO.java
@@ -11,6 +11,7 @@ import java.math.BigDecimal;
 @AllArgsConstructor
 @Data
 public class UserResponseDTO {
+    private Long id;
     private String firstName;
     private String lastName;
     private String document;
