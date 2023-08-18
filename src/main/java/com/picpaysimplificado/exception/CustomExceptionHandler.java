010e89c66f091197695c27ad037685adae456f25
Anderson Freitas <andersonfreitas21@gmail.com>
2023-08-18 17:46:35 -0300

    feat: custom exception handler

--- src/main/java/com/picpaysimplificado/exception/CustomExceptionHandler.java
+++ src/main/java/com/picpaysimplificado/exception/CustomExceptionHandler.java
@@ -3,15 +3,24 @@ package com.picpaysimplificado.exception;
 import com.picpaysimplificado.exception.response.ApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
-@ControllerAdvice
+@RestControllerAdvice
 public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(ResourceNotFoundException.class)
     public final ResponseEntity<ApiResponse> handleResourceNotFoundException(ResourceNotFoundException exception){
         return new ResponseEntity<>(new ApiResponse(exception.getMessage(), LocalDateTime.now()), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(InvalidAuthenticationException.class)
+    public final ResponseEntity<ApiResponse> handleInvalidAuthenticationException(InvalidAuthenticationException exception){
+        return new ResponseEntity<>(new ApiResponse(exception.getMessage(), LocalDateTime.now()), HttpStatus.UNAUTHORIZED);
+    }
+    @ExceptionHandler(ValueMismatchException.class)
+    public final ResponseEntity<ApiResponse> handleValueMismatchException(ValueMismatchException exception){
+        return new ResponseEntity<>(new ApiResponse(exception.getMessage(), LocalDateTime.now()), HttpStatus.BAD_REQUEST);
     }
 }
---
+++ src/main/java/com/picpaysimplificado/exception/ValueMismatchException.java
@@ -0,0 +1,7 @@
+package com.picpaysimplificado.exception;
+
+public class ValueMismatchException extends RuntimeException{
+    public ValueMismatchException(String message) {
+        super(message);
+    }
+}
