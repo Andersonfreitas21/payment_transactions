43be8ac37fe9d65306ef179741785c1267099dec
Anderson Freitas <andersonfreitas21@gmail.com>
2023-08-21 17:00:55 -0300

    feat: Rest controller advice

--- src/main/java/com/picpaysimplificado/exception/CustomExceptionHandler.java
+++ src/main/java/com/picpaysimplificado/exception/CustomExceptionHandler.java
@@ -1,21 +1,28 @@
 package com.picpaysimplificado.exception;
 
 import com.picpaysimplificado.exception.response.ApiResponse;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
-@ControllerAdvice
+@RestControllerAdvice
 public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public final ResponseEntity<ApiResponse> handleResourceExistsException(DataIntegrityViolationException exception){
+        return new ResponseEntity<>(new ApiResponse(exception.getMessage(), LocalDateTime.now()), HttpStatus.CONFLICT);
+    }
+
     @ExceptionHandler(ResourceNotFoundException.class)
     public final ResponseEntity<ApiResponse> handleResourceNotFoundException(ResourceNotFoundException exception){
         return new ResponseEntity<>(new ApiResponse(exception.getMessage(), LocalDateTime.now()), HttpStatus.NOT_FOUND);
     }
-    @ExceptionHandler(InvalidAuthorizationException.class)
-    public final ResponseEntity<ApiResponse> handleInvalidAuthenticationException(InvalidAuthorizationException exception){
+    @ExceptionHandler(InvalidAuthenticationException.class)
+    public final ResponseEntity<ApiResponse> handleInvalidAuthenticationException(InvalidAuthenticationException exception){
         return new ResponseEntity<>(new ApiResponse(exception.getMessage(), LocalDateTime.now()), HttpStatus.UNAUTHORIZED);
     }
 
