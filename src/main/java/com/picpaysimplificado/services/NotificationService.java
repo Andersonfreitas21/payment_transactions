ca362c156a8f6fd7cc8b7aa9de8a2fe7c2caace5
Anderson Freitas <andersonfreitas21@gmail.com>
2023-08-21 16:08:39 -0300

    feat: changing notification url

--- src/main/java/com/picpaysimplificado/services/NotificationService.java
+++ src/main/java/com/picpaysimplificado/services/NotificationService.java
@@ -2,6 +2,7 @@ package com.picpaysimplificado.services;
 
 import com.picpaysimplificado.domain.dtos.NotificationDTO;
 import com.picpaysimplificado.domain.user.User;
+import com.picpaysimplificado.exception.NotificationIsDownException;
 import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,20 +18,19 @@ import java.util.Objects;
 public class NotificationService {
 
     private static final Logger log = LoggerFactory.getLogger(NotificationService.class);
-    private static final String URL_NOTIFY = "http://o4d9z.mocklab.io/notify";
+    private static final String URL_NOTIFY = "https://o368l.wiremockapi.cloud/notify";
     private final RestTemplate restTemplate;
 
-    public void sendNotification(User user, String message) throws Exception {
+    public void sendNotification(User user, String message) {
         String email = user.getEmail();
         NotificationDTO notificationRequest = new NotificationDTO(email, message);
 
         ResponseEntity<String> notificationResponse = restTemplate.postForEntity(URL_NOTIFY, notificationRequest, String.class);
         log.info("Info log message " + Objects.requireNonNull(notificationResponse.getBody()).equalsIgnoreCase("message"));
 
-        if(!(notificationResponse.getStatusCode() == HttpStatus.OK)){
+        if (!(notificationResponse.getStatusCode() == HttpStatus.OK)) {
             log.error("Serviço de notificação está fora do ar");
-            throw new Exception("Notification service is down");
-
+            throw new NotificationIsDownException("Notification service is down");
         }
 
     }
